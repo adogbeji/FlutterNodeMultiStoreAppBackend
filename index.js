6c48e0e78f54ded43db30c428cdccea8102d9c60
adogbeji <baka_1993@yahoo.co.uk>
2024-12-28 14:55:41 +0000

    Create Express application & start listening for incoming requests

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
 const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, '0.0.0.0', function() {
+    console.log(`Server is running on port ${PORT}...`);
+});
